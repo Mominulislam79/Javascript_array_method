ed59db240a9f50cf22d368768e41e5eba43e5d08
Md. Mominul Islam <mominulcuet79@gmail.com>
2024-07-18 14:15:18 +0600

    This repo is for practicing different array methods in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+//Practice Example-01: push() and pop() method
+let users=["Ekhlas","Rony","Azam","Idris","Polash","Babu","Ferdous"];
+// adding a new user
+users.push("Raja");
+console.log(users);
+// removing the last user
+let Lastuser= users.pop();
+console.log(Lastuser);
+console.log(users);
+
